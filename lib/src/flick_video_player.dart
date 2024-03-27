3be754c12d07a88d54e62ecc87563a9f0a0a7b1f
Sochea <socheatech@gmail.com>
2024-03-27 14:23:47 +0700

    update version wakelock

--- lib/src/flick_video_player.dart
+++ lib/src/flick_video_player.dart
@@ -4,7 +4,7 @@ import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:wakelock/wakelock.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 class FlickVideoPlayer extends StatefulWidget {
   const FlickVideoPlayer({
@@ -81,7 +81,7 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer> {
     _setPreferredOrientation();
 
     if (widget.wakelockEnabled) {
-      Wakelock.enable();
+      WakelockPlus.enable();
     }
 
     if (kIsWeb) {
@@ -96,7 +96,7 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer> {
   void dispose() {
     flickManager.flickControlManager!.removeListener(listener);
     if (widget.wakelockEnabled) {
-      Wakelock.disable();
+      WakelockPlus.disable();
     }
     super.dispose();
   }
@@ -114,8 +114,8 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer> {
   _switchToFullscreen() {
     if (widget.wakelockEnabledFullscreen) {
       /// Disable previous wakelock setting.
-      Wakelock.disable();
-      Wakelock.enable();
+      WakelockPlus.disable();
+      WakelockPlus.enable();
     }
 
     _isFullscreen = true;
@@ -134,14 +134,14 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer> {
       );
     });
 
-    Overlay.of(context)!.insert(_overlayEntry!);
+    Overlay.of(context).insert(_overlayEntry!);
   }
 
   _exitFullscreen() {
     if (widget.wakelockEnabled) {
       /// Disable previous wakelock setting.
-      Wakelock.disable();
-      Wakelock.enable();
+      WakelockPlus.disable();
+      WakelockPlus.enable();
     }
 
     _isFullscreen = false;
